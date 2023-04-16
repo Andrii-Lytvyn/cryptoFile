442cb6c3a05cc09e0e7288c27f372faf98cc1c7b
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-04-16 14:39:58 +0200

    Add crypto v1

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,62 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         File cryptoFile = new File("src/res/crypto.txt");
-        addInfoToFile(cryptoFile);
+        List<String> crypto = new ArrayList<>();
+        crypto.add("hello");
+        crypto.add("Hello world");
+        crypto.add("!1");
+        makeCrypto(crypto);
+
+        //addInfoToFile(cryptoFile);
     }
+
+    private static void makeCrypto(List<String> list) {
+        char tempChar = 0;
+        char newChar = 0;
+        String tempString = "";
+        int code;
+        int j = 0;
+
+        for (String items : list) {
+
+            for (int i = 0; i < items.length(); i++) {
+                tempChar = items.charAt(i);
+                code = (int) tempChar;
+                code++;
+                newChar = (char) code;
+                tempString += newChar;
+            }
+            list.set(j, tempString);
+            j++;
+
+
+        }
+
+
+    }
+
 
     private static void addInfoToFile(File cryptoFile) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<String> list = new ArrayList<>();
-         int counter=1;
+        int counter = 1;
         System.out.println("Нажмите '1' для выхода");
         while (true) {
-            System.out.print("Введите запись №" + counter+": ");
+            System.out.print("Введите запись №" + counter + ": ");
             String line = br.readLine();
             if (!line.equals("1")) {
                 list.add(line);
                 counter++;
             } else
-                           break;
+                break;
 
         }
+        makeCrypto(list);
         makeOutputFile(list);
 
 
     }
+
 
     public void parseTaskFromFile(File tasksFile) throws IOException {
         BufferedReader inputFileReader = new BufferedReader(new FileReader(tasksFile));
@@ -52,9 +86,8 @@ public class Main {
     public static void makeOutputFile(List<String> list) throws IOException {
         try {
             FileWriter cryptoFile = new FileWriter("src/res/crypto.txt");
-            for (String items : list)
-            {
-                cryptoFile.write(items);
+            for (String items : list) {
+                cryptoFile.write(items + "\n");
             }
 
             cryptoFile.close();
